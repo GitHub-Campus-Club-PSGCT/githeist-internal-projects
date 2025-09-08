4fcf1a67a3e7a4040bbeb00494d5b97645e8a34e
Subhasree L K <lksubhasree@gmail.com>
2025-09-08 20:51:19 +0530

    Delete cleanup_tools (Owner: Darshan) directory

--- cleanup_tools (Owner: Darshan)/log_cleaner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# TechSphere Corporation - Log Cleanup Utility
-# WARNING: Irreversible deletion
-
-TARGET_DIR=/var/logs/archive
-echo "Cleaning logs from $TARGET_DIR"
-rm -rf $TARGET_DIR/*
-echo "Cleanup finished at $(date)"
